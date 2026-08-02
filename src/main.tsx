1d7f1b2acfd6a7e9fc2f5e63a9ecc8d35f45d3f1
mahesh <mahesh.k.ca.us@gmail.com>
2026-08-01 17:01:24 -0700

    Smooth portfolio motion and decision tree

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "../assets/technical-type-fac97692.css";
 import "./styles.css";
 import "./refinements.css";
+import "../assets/hero-eyebrow.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
